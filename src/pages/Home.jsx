304f1274f60496c97d116f7889c6a13d2d0495eb
AndyJrII <enehizenajames@gmail.com>
2024-07-13 13:24:15 +0100

    Added Youtube Video to Why join Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,143 +7,136 @@ import Footer from '../components/footer/Footer';
 import Hero from '../components/carousels/Hero';
 import LatestBlogs from '../components/LatestBlogs';
 import { motion } from 'framer-motion';
-import { FaCertificate, FaLaptop, FaList, FaGlobe } from 'react-icons/fa';
 
 const Home = () => {
   return (
-    <section className='home-bg'>
+    <section>
       <Navigation />
       <main>
         <Hero />
-
-        <section className='page-section'>
-          <p className='h1 mb-2 text-center'>
-            <span className='badge text-white my-2 tomato text-wrap'>
-              Why you should Get Nerdified!
-            </span>
-          </p>
-
-          <div className='container'>
-            <div className='row justify-content-center'>
-              <div className='col-md-5 why-container rounded-3'>
-                <h2 className='why-h2 mb-4'>
-                  <FaCertificate className='why-icon' />
-                  We have Expert Instructors
-                </h2>
-                <h2 className='why-h2'>
-                  <FaGlobe className='why-icon' />
-                  Interactive Live Online Classes
-                </h2>
-              </div>
-              <div className='col-md-5 why-container rounded-3'>
-                <h2 className='why-h2 mb-4'>
-                  <FaList className='why-icon' />
-                  Structured Curriculum
-                </h2>
-                <h2 className='why-h2'>
-                  <FaLaptop className='why-icon' />
-                  Practical Hands-On Projects
-                </h2>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <section className='page-section clearfix'>
-          <div className='container'>
-            <div className='intro'>
-              <motion.img
-                whileInView={{ scale: 1 }}
-                initial={{ scale: 0 }}
-                transition={{ type: 'tween', duration: 2 }}
-                className='intro-img img-fluid mb-lg-0 rounded'
-                src={Marketing1}
-                alt='...'
-                loading='lazy'
-              />
-              <motion.div
-                whileInView={{ scale: 1 }}
-                initial={{ scale: 0 }}
-                transition={{ type: 'tween', duration: 2 }}
-                className='intro-text left-0 text-center bg-faded p-5 rounded'
-              >
-                <h2 className='section-heading mb-4'>
-                  <span className='section-heading-upper'>CODING ACADEMY</span>
-                  <span className='section-heading-lower'>
-                    LEARN TO CODE ONLINE & BUILD AMAZING SOFTWARE
-                  </span>
-                </h2>
-                <p className='mb-3'>
-                  We offer a variety of online programming classes for all skill
-                  levels, from beginner to advanced. Our classes are taught by
-                  experienced & qualified instructors who will help you learn
-                  the skills you need to build amazing software.
-                </p>
-                <div className='intro-button mx-auto'>
-                  <Link className='btn btn-xl text-white' to='courses'>
-                    Enroll now!
-                  </Link>
-                </div>
-              </motion.div>
-            </div>
-          </div>
-        </section>
-
-        <section className='page-section clearfix'>
-          <div className='container'>
-            <div className='intro'>
-              <motion.img
-                whileInView={{ scale: 1 }}
-                initial={{ scale: 0 }}
-                transition={{ type: 'tween', duration: 2 }}
-                className='intro-img img-fluid mb-lg-0 rounded'
-                src={Marketing2}
-                alt='...'
-                loading='lazy'
-              />
-              <motion.div
-                whileInView={{ scale: 1 }}
-                initial={{ scale: 0 }}
-                transition={{ type: 'tween', duration: 2 }}
-                className='intro-text left-0 text-center bg-faded p-5 rounded'
-              >
-                <h2 className='section-heading mb-4'>
-                  <span className='section-heading-upper'>
-                    Amazing Websites
-                  </span>
-                  <span className='section-heading-lower'>
-                    Build amazing websites at an affordable price
-                  </span>
-                </h2>
-                <p className='mb-3'>
-                  We pride ourselves in building websites that meet clients'
-                  needs. We believe that the best websites are the ones that are
-                  tailored to each individual client. We'll work closely with
-                  you to understand your business goals & create a site that's
-                  both beautiful & functional.
-                </p>
-                <div className='intro-button mx-auto'>
-                  <Link
-                    className='btn btn-xl text-white'
-                    to='https://www.github.com/andyjrii'
-                  >
-                    Visit GitHub!
-                  </Link>
-                </div>
-              </motion.div>
-            </div>
-          </div>
-        </section>
-
-        <section className='page-section clearfix'>
-          <div className='blog_section text-center'>
-            <p className='h1'>
-              <span className='badge text-white my-2 tomato'>Latest Posts</span>
+        <section className='home-bg'>
+          <section className='page-section'>
+            <p className='h1 mb-2 text-center'>
+              <span className='badge text-white my-2 tomato text-wrap'>
+                Why Enroll in Our Programming Classes?
+              </span>
             </p>
-            <LatestBlogs />
-          </div>
-        </section>
 
+            <div className='container'>
+              <div className='row justify-content-center'>
+                <iframe
+                  className='rounded-4'
+                  style={{ border: 'none' }}
+                  width='600'
+                  height='400'
+                  src='https://www.youtube.com/embed/819zkRMjn4k?si=1E5f2zphhW52I_6D'
+                  title='Why Get Nerdified?'
+                  referrerPolicy='strict-origin-when-cross-origin'
+                  allowFullScreen
+                ></iframe>
+              </div>
+            </div>
+          </section>
+
+          <section className='page-section clearfix'>
+            <div className='container'>
+              <div className='intro'>
+                <motion.img
+                  whileInView={{ scale: 1 }}
+                  initial={{ scale: 0 }}
+                  transition={{ type: 'tween', duration: 2 }}
+                  className='intro-img img-fluid mb-lg-0 rounded'
+                  src={Marketing1}
+                  alt='...'
+                  loading='lazy'
+                />
+                <motion.div
+                  whileInView={{ scale: 1 }}
+                  initial={{ scale: 0 }}
+                  transition={{ type: 'tween', duration: 2 }}
+                  className='intro-text left-0 text-center bg-faded p-5 rounded'
+                >
+                  <h2 className='section-heading mb-4'>
+                    <span className='section-heading-upper'>
+                      CODING ACADEMY
+                    </span>
+                    <span className='section-heading-lower'>
+                      LEARN TO CODE ONLINE & BUILD AMAZING SOFTWARE
+                    </span>
+                  </h2>
+                  <p className='mb-3'>
+                    We offer a variety of online programming classes for all
+                    skill levels, from beginner to advanced. Our classes are
+                    taught by experienced & qualified instructors who will help
+                    you learn the skills you need to build amazing software.
+                  </p>
+                  <div className='intro-button mx-auto'>
+                    <Link className='btn btn-xl text-white' to='courses'>
+                      Enroll now!
+                    </Link>
+                  </div>
+                </motion.div>
+              </div>
+            </div>
+          </section>
+
+          <section className='page-section clearfix'>
+            <div className='container'>
+              <div className='intro'>
+                <motion.img
+                  whileInView={{ scale: 1 }}
+                  initial={{ scale: 0 }}
+                  transition={{ type: 'tween', duration: 2 }}
+                  className='intro-img img-fluid mb-lg-0 rounded'
+                  src={Marketing2}
+                  alt='...'
+                  loading='lazy'
+                />
+                <motion.div
+                  whileInView={{ scale: 1 }}
+                  initial={{ scale: 0 }}
+                  transition={{ type: 'tween', duration: 2 }}
+                  className='intro-text left-0 text-center bg-faded p-5 rounded'
+                >
+                  <h2 className='section-heading mb-4'>
+                    <span className='section-heading-upper'>
+                      Amazing Websites
+                    </span>
+                    <span className='section-heading-lower'>
+                      Build amazing websites at an affordable price
+                    </span>
+                  </h2>
+                  <p className='mb-3'>
+                    We pride ourselves in building websites that meet clients'
+                    needs. We believe that the best websites are the ones that
+                    are tailored to each individual client. We'll work closely
+                    with you to understand your business goals & create a site
+                    that's both beautiful & functional.
+                  </p>
+                  <div className='intro-button mx-auto'>
+                    <Link
+                      className='btn btn-xl text-white'
+                      to='https://www.github.com/andyjrii'
+                    >
+                      Visit GitHub!
+                    </Link>
+                  </div>
+                </motion.div>
+              </div>
+            </div>
+          </section>
+
+          <section className='page-section clearfix'>
+            <div className='blog_section text-center'>
+              <p className='h1'>
+                <span className='badge text-white my-2 tomato'>
+                  Latest Posts
+                </span>
+              </p>
+              <LatestBlogs />
+            </div>
+          </section>
+        </section>
         <Footer />
       </main>
     </section>
